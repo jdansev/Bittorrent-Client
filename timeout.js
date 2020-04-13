523028d86ca655338c135bc0065648374dd1850b
John Daniel Sevilla <jdsevilla1996@gmail.com>
2020-04-13 13:15:46 +1000

    Removed unused timeout function

--- timeout.js
+++ timeout.js
@@ -1,21 +1,4 @@
 
-export const createSocketTimeout = (timeoutName, socket, peer, TIMEOUT_MS) => {
-    let flag = false;
-
-    return {
-        start: () => setTimeout(() => {
-            if (!flag) {
-                console.log(`${timeoutName} ${peer.ip}:${peer.port}`);
-                socket.destroy();
-            }
-        }, TIMEOUT_MS),
-
-        clear: () => flag = true
-    }
-}
-
-
-
 export class SocketTimeout {
 
     constructor(timeoutName, socket, peer) {
